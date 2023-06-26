fc9825f561a341fd6e4423aec9c9f9ad84d8ac32
Olamilekan <nunuolamilekan@gmail.com>
2023-06-26 13:12:53 +0100

    updated sanity

---
+++ src/pages/admin/[[...index]].tsx
@@ -0,0 +1,8 @@
+import { NextStudio } from "next-sanity/studio";
+import config from "../../../sanity.config";
+
+export default function AdminPage() {
+  return (
+    <NextStudio config={config} />
+  );
+}
--- src/schemas/service.ts
+++ src/schemas/service.ts
@@ -1,22 +1,26 @@
-export default {
-  name: "servies",
-  title: "Servies",
+import { defineField, defineType } from "sanity";
+
+const service = defineType({
+  name: "services",
+  title: "Services",
   type: "document",
   fields: [
-    {
+    defineField({
       name: "title",
       title: "Title",
       type: "string",
-    },
-    {
+    }),
+    defineField({
       name: "image",
       title: "Image",
       type: "image",
-    },
-    {
+    }),
+    defineField({
       name: "description",
       title: "Description",
       type: "string",
-    },
+    }),
   ],
-};
+});
+
+export default service;
